afb4e46e5474c3c6a7fb10850950b285bd880c77
naslth <letrunghieu10x@gmail.com>
2021-12-23 16:07:13 +0700

    reset score

--- desktop/src/com/bom/game/desktop/DesktopLauncher.java
+++ desktop/src/com/bom/game/desktop/DesktopLauncher.java
@@ -5,11 +5,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.bom.game.BomGame;
 
 public class DesktopLauncher {
-    public static void main(String[] arg) {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.width = 17 * 64;
-        config.height = 13 * 64;
-        config.resizable = false;
-        new LwjglApplication(new BomGame(), config);
-    }
+	public static void main(String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 17 * 48;
+		config.height = 13 * 48;
+		config.resizable = false;
+		new LwjglApplication(new BomGame(), config);
+	}
 }
